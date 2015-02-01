6ac3db332a41ead9814c67653542251020dd739b
Rich Leland <rich.leland@messagesystems.com>
2015-02-01 14:23:45 -0500

    added tests for base class

---
+++ test/test_base.py
@@ -0,0 +1,62 @@
+import pytest
+import responses
+
+from sparkpost.base import Resource
+from sparkpost.exceptions import SparkPostAPIException
+
+
+fake_base_uri = 'https://fake-base.com'
+fake_api_key = 'fake-api-key'
+fake_resource_key = 'fake-resource-key'
+fake_uri = "%s/%s" % (fake_base_uri, fake_resource_key)
+
+
+def create_resource():
+    resource = Resource(fake_base_uri, fake_api_key)
+    resource.key = fake_resource_key
+    return resource
+
+
+def test_uri():
+    resource = create_resource()
+    assert resource.uri == fake_uri
+
+
+@responses.activate
+def test_success_request():
+    responses.add(
+        responses.GET,
+        fake_uri,
+        status=200,
+        content_type='application/json',
+        body='{"results": []}'
+    )
+    resource = create_resource()
+    results = resource.request('GET', resource.uri)
+    assert results == []
+
+
+@responses.activate
+def test_fail_request():
+    responses.add(
+        responses.GET,
+        fake_uri,
+        status=500,
+        content_type='application/json',
+        body='{"errors": [{"message": "failure"}]}'
+    )
+    resource = create_resource()
+    with pytest.raises(SparkPostAPIException):
+        resource.request('GET', resource.uri)
+
+
+def test_fail_get():
+    resource = create_resource()
+    with pytest.raises(NotImplementedError):
+        resource.get()
+
+
+def test_fail_list():
+    resource = create_resource()
+    with pytest.raises(NotImplementedError):
+        resource.list()
---
+++ test/test_metrics.py
@@ -0,0 +1,61 @@
+import pytest
+import responses
+
+from sparkpost import SparkPost
+from sparkpost.exceptions import SparkPostAPIException
+
+
+@responses.activate
+def test_success_campaigns():
+    responses.add(
+        responses.GET,
+        'https://api.sparkpost.com/api/v1/metrics/campaigns',
+        status=200,
+        content_type='application/json',
+        body='{"results": {"campaigns": []}}'
+    )
+    sp = SparkPost('fake-key')
+    results = sp.metrics.campaigns.list()
+    assert results == []
+
+
+@responses.activate
+def test_fail_campaigns():
+    responses.add(
+        responses.GET,
+        'https://api.sparkpost.com/api/v1/metrics/campaigns',
+        status=500,
+        content_type='application/json',
+        body='{"errors": [{"message": "You failed"}]}'
+    )
+    with pytest.raises(SparkPostAPIException):
+        sp = SparkPost('fake-key')
+        sp.metrics.campaigns.list()
+
+
+@responses.activate
+def test_success_domains():
+    responses.add(
+        responses.GET,
+        'https://api.sparkpost.com/api/v1/metrics/domains',
+        status=200,
+        content_type='application/json',
+        body='{"results": {"domains": []}}'
+    )
+    sp = SparkPost('fake-key')
+    results = sp.metrics.domains.list()
+    assert results == []
+
+
+@responses.activate
+def test_fail_domains():
+    responses.add(
+        responses.GET,
+        'https://api.sparkpost.com/api/v1/metrics/domains',
+        status=500,
+        content_type='application/json',
+        body='{"errors": [{"message": "You failed"}]}'
+    )
+    with pytest.raises(SparkPostAPIException):
+        sp = SparkPost('fake-key')
+        sp.metrics.domains.list()
