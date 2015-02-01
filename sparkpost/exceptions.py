6a55063a8817bc9875e35d67956c057bc4f5fd01
Rich Leland <rich.leland@messagesystems.com>
2015-02-01 11:37:08 -0500

    handle API exceptions

--- sparkpost/exceptions.py
+++ sparkpost/exceptions.py
@@ -4,4 +4,15 @@ class SparkPostException(Exception):
 
 class SparkPostAPIException(SparkPostException):
     "Handle 4xx and 5xx errors from the SparkPost API"
-    pass
+    def __init__(self, response, *args, **kwargs):
+        errors = response.json()['errors']
+        errors = [e['message'] for e in errors]
+        message = """Call to {uri} returned {status_code}, errors:
+
+        {errors}
+        """.format(
+            uri=response.url,
+            status_code=response.status_code,
+            errors='\n'.join(errors)
+        )
+        super(SparkPostAPIException, self).__init__(message, *args, **kwargs)
