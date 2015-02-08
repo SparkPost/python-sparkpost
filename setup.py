54bb7b42a58b181da21c6d370696faaec797de80
Rich Leland <rich.leland@messagesystems.com>
2015-02-08 18:33:11 -0500

    added wheel support, prep for PyPI

--- setup.py
+++ setup.py
@@ -1,4 +1,8 @@
-from distutils.core import setup
+from codecs import open
+from setuptools import setup
+
+with open('README.rst', 'r', 'utf-8') as f:
+    readme = f.read()
 
 setup(
     name='sparkpost',
@@ -9,7 +13,16 @@ setup(
     url='https://github.com/SparkPost/python-sparkpost',
     license='Apache 2.0',
     description='SparkPost Python API client',
-    long_description=open('README.rst').read(),
+    long_description=readme,
     install_requires=['requests==2.5.1'],
-    zip_safe=False,
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Developers',
+        'Topic :: Communications :: Email',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+    ],
 )
