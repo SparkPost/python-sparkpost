c3a5400b38474921231686e7dd52234cc74d8b4e
mhossain <mohammad.hossain@sparkpost.com>
2015-10-27 17:58:32 -0400

    Add django backend Add unit tests

---
+++ sparkpost/django/email_backend.py
@@ -0,0 +1,43 @@
+from django.core.mail.backends.base import BaseEmailBackend
+# from django.core.mail import EmailMultiAlternatives
+from sparkpost import SparkPost
+from django.conf import settings
+
+
+class SparkPostEmailBackend(BaseEmailBackend):
+    """
+    SparkPost wrapper for Django email backend
+    """
+
+    def __init__(self, fail_silently=False, **kwargs):
+        super(SparkPostEmailBackend, self)\
+            .__init__(fail_silently=fail_silently, **kwargs)
+
+        sp_password = getattr(settings, 'SP_PASSWORD', None)
+
+        self.client = SparkPost(sp_password)
+
+    def send_messages(self, email_messages):
+        """
+        Send emails, returns integer representing number of successful emails
+        """
+
+        success = 0
+        for message in email_messages:
+            try:
+                response = self._send(message)
+                success += response['total_accepted_recipients']
+            except Exception:
+                if not self.fail_silently:
+                    raise
+        return success
+
+    def _send(self, message):
+        args = dict(
+            recipients=message.to,
+            html=message.body,
+            from_email=message.from_email,
+            subject=message.subject
+        )
+
+        return self.client.transmissions.send(**args)
---
+++ test/django/test_email_backend.py
@@ -0,0 +1,93 @@
+from sparkpost.django.email_backend import SparkPostEmailBackend
+from sparkpost.transmissions import Transmissions
+from django.conf import settings
+from django.core.mail import send_mail, send_mass_mail
+import pytest
+
+import mock
+
+API_KEY = 'API_Key'
+
+settings.configure(
+    DEBUG=True,
+    EMAIL_BACKEND='sparkpost.django.email_backend.SparkPostEmailBackend',
+    SP_PASSWORD=API_KEY
+)
+
+
+def mailer(to=None, fail_silently=False):
+    if not to:
+        to = ['to@example.com']
+
+    return send_mail(subject='test subject',
+                     message='test body',
+                     from_email='from@example.com',
+                     recipient_list=to,
+                     fail_silently=fail_silently)
+
+
+def test_password_retrieval():
+    backend = SparkPostEmailBackend()
+    assert backend.client.api_key == API_KEY
+
+
+def test_fail_silently():
+    # should not raise
+    with mock.patch.object(Transmissions, 'send') as mock_send:
+        mock_send.side_effect = Exception('i should not be raised')
+        mailer(fail_silently=True)
+
+    # should raise
+    with mock.patch.object(Transmissions, 'send') as mock_send:
+        mock_send.side_effect = Exception('i should be raised')
+        with pytest.raises(Exception):
+            mailer()
+
+
+def test_successful_sending():
+    with mock.patch.object(Transmissions, 'send') as mock_send:
+        mock_send.return_value = {'total_accepted_recipients': 1,
+                                  'total_rejected_recipients': 2}
+
+        result = mailer(to=['to1@example.com', 'to2@example.com'],
+                        fail_silently=True)
+
+        assert result == 1
+
+    with mock.patch.object(Transmissions, 'send') as mock_send:
+        mock_send.return_value = {'total_accepted_recipients': 10,
+                                  'total_rejected_recipients': 2}
+
+        result = mailer(to=['to1@example.com', 'to2@example.com'],
+                        fail_silently=True)
+
+        assert result == 10
+
+
+def test_send_number_of_emails_correctly():
+    with mock.patch.object(Transmissions, 'send') as mock_send:
+        mailer(to=['to1@example.com', 'to2@example.com'], fail_silently=True)
+        assert mock_send.call_count == 1
+
+    with mock.patch.object(Transmissions, 'send') as mock_send:
+        message1 = ('message 1 subject', 'message 1 body', 'from@example.com',
+                    ['to1@example.com'])
+        message2 = ('message 2 subject', 'message 2 body', 'from@example.com',
+                    ['to2@example.com'])
+        message3 = ('message 3 subject', 'message 3 body', 'from@example.com',
+                    ['to3@example.com'])
+
+        send_mass_mail((message1, message2, message3), fail_silently=False)
+        assert mock_send.call_count == 3
+
+
+def test_params():
+    recipients = ['to1@example.com', 'to2@example.com']
+    with mock.patch.object(Transmissions, 'send'):
+        mailer(to=recipients, fail_silently=True)
+
+        Transmissions.send.assert_called_with(recipients=recipients,
+                                              html='test body',
+                                              from_email='from@example.com',
+                                              subject='test subject'
+                                              )
