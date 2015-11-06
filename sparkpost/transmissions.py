1193d62bfb7c57be1ae89930217cca4776165138
Rich Leland <rich.leland@messagesystems.com>
2015-11-06 10:27:39 -0500

    Add docstrings for additional options

--- sparkpost/transmissions.py
+++ sparkpost/transmissions.py
@@ -86,6 +86,8 @@ class Transmissions(Resource):
         :param dict substitution_data: Corresponds to substitutions in
             html/text content. See `substitutions reference
             <https://www.sparkpost.com/docs/substitutions-reference>`_.
+        :param str start_time: Delay generation of messages until this
+            datetime.
         :param bool track_opens: Defaults to True. Used to track opens of
             transmission
         :param bool track_clicks: Defaults to True. Used to track clicks of
@@ -93,6 +95,11 @@ class Transmissions(Resource):
         :param bool use_sandbox: Flag must be set to use sandbox domain instead
             of verified sending domain. Limited to a lifetime of 50
             transmissions with this domain
+        :param bool transactional: Whether message is transactional or
+            non-transactional for unsubscribe and suppression purposes
+        :param bool skip_suppression: Whether or not to ignore customer
+            suppression rules, for this transmission only. Only applicable if
+            your configuration supports this parameter. (SparkPost Elite only)
         :param dict custom_headers: Used to set any headers associated with
             transmission
 
