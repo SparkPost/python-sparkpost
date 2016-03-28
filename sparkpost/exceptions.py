85bed2102c96cb8999a0d2de6ea1295a4b798013
pegler <matt@goteamup.com>
2016-03-28 10:14:02 -0400

    handle API error responses where there is no json content - often 504 errors

--- sparkpost/exceptions.py
+++ sparkpost/exceptions.py
@@ -5,9 +5,13 @@ class SparkPostException(Exception):
 class SparkPostAPIException(SparkPostException):
     "Handle 4xx and 5xx errors from the SparkPost API"
     def __init__(self, response, *args, **kwargs):
-        errors = response.json()['errors']
-        errors = [e['message'] + ': ' + e.get('description', '')
-                  for e in errors]
+        self.response_status_code = response.status_code
+        if 'application/json' in response.headers['content-type'].lower():
+          errors = response.json()['errors']
+          errors = [e['message'] + ': ' + e.get('description', '')
+                    for e in errors]
+        else:
+          errors = ['Not available - no JSON response']
         message = """Call to {uri} returned {status_code}, errors:
 
         {errors}
