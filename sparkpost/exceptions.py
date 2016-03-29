5a95fda4e9448af16a12a7a7244a1f0b96cae0ab
pegler <matt@goteamup.com>
2016-03-29 14:19:39 -0400

    flakes

--- sparkpost/exceptions.py
+++ sparkpost/exceptions.py
@@ -7,13 +7,15 @@ class SparkPostAPIException(SparkPostException):
     def __init__(self, response, *args, **kwargs):
         self.response_status_code = response.status_code
         self.response_error_codes = []
-        if 'application/json' in response.headers.get('content-type','').lower():
-          errors = response.json()['errors']
-          self.response_error_codes = [e['code'] for e in errors if 'code' in e]
-          errors = [e['message'] + ': ' + e.get('description', '')
-                    for e in errors]
+        content_type = response.headers.get('content-type', '').lower()
+        if 'application/json' in content_type:
+            errors = response.json()['errors']
+            self.response_error_codes = [e['code'] for e in errors
+                                         if 'code' in e]
+            errors = [e['message'] + ': ' + e.get('description', '')
+                      for e in errors]
         else:
-          errors = ['Not available - no JSON response']
+            errors = ['Not available - no JSON response']
         message = """Call to {uri} returned {status_code}, errors:
 
         {errors}
