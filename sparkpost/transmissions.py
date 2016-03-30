daaa7db44f7dad663d5e3a21b7178cb7b6e45229
Rich Leland <rich.leland@messagesystems.com>
2016-03-30 11:14:30 -0400

    Add option for ip_pool on Transmissions

--- sparkpost/transmissions.py
+++ sparkpost/transmissions.py
@@ -40,6 +40,7 @@ class Transmissions(Resource):
         model['options']['transactional'] = kwargs.get('transactional')
         model['options']['sandbox'] = kwargs.get('use_sandbox')
         model['options']['skip_suppression'] = kwargs.get('skip_suppression')
+        model['options']['ip_pool'] = kwargs.get('ip_pool')
         model['options']['inline_css'] = kwargs.get('inline_css')
 
         model['content']['use_draft_template'] = \
@@ -192,6 +193,8 @@ class Transmissions(Resource):
         :param bool skip_suppression: Whether or not to ignore customer
             suppression rules, for this transmission only. Only applicable if
             your configuration supports this parameter. (SparkPost Elite only)
+        :param str ip_pool: The name of a dedicated IP pool associated with
+            your account
         :param bool inline_css: Whether or not to perform CSS inlining
         :param dict custom_headers: Used to set any headers associated with
             transmission
