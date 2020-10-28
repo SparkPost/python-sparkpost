9f119efb2cbddc5b891654efc0185cfc96a52978
Wyn Bennett <wyn@simondata.com>
2020-10-28 14:35:06 -0400

    Fixed the subaccounts endpoint to provide the key of 'subaccounts' when making the uri for a request

--- sparkpost/subaccounts.py
+++ sparkpost/subaccounts.py
@@ -5,10 +5,11 @@ from .base import Resource
 class Subaccounts(Resource):
     """
     Domains class for managing subaccounts.
-    For detailed request and response formats, see the 
+    For detailed request and response formats, see the
     `Subaccounts API documentation
     <https://developers.sparkpost.com/api/subaccounts/>`.
     """
+    key = 'subaccounts'
 
     def create(self, **kwargs):
         """
@@ -16,7 +17,7 @@ class Subaccounts(Resource):
         :param str name: subaccount name
         :param str ip_pool: ID of an IP Pool in which to restrict this subaccount's mail deliveries
         :param bool setup_api_key: Whether or not to create an API key for the subaccount.
-        :param str key_label: User friendly identifier for the initial subaccount api key. 
+        :param str key_label: User friendly identifier for the initial subaccount api key.
         :param list key_grants: List of grants to give to the initial subaccount api key.
         :param list key_valid_ips: List of IP's that the initial subaccount API key can be used from.
         :returns: Subaccount's ID, along with the API key and API key label, if one was created.
@@ -54,7 +55,7 @@ class Subaccounts(Resource):
         :param str name: subaccount name
         :param str status: Status of the subaccount
         :param str ip_pool: ID of an IP Pool in which to restrict this subaccount's mail deliveries
-        
+
         :returns: Message detailing the success of the operation.
         :raises: :exc:`SparkPostAPIException` if API call fails
         """
